f9818788ad87e503cc2197d8dc091ea1883ba797
Dominic Patrick <dom.patrick92@gmail.com>
2021-03-26 18:14:45 -0400

    signup page setup

--- authenticate-me/frontend/src/components/LoginFormPage/index.js
+++ authenticate-me/frontend/src/components/LoginFormPage/index.js
@@ -30,25 +30,27 @@ function LoginFormPage() {
         <ul>
           {errors.map((error, idx) => <li key={idx}>{error}</li>)}
         </ul>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
+        <div className="formContainer">
+          <label>
+            Username or Email
+            <input
+              type="text"
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Log In</button>
+        </div>
       </form>
     );
   }
